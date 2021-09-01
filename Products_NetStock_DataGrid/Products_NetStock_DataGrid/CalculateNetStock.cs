2abeb3caa0a7266ea1672af4ed96664488ae8138
m-a-d-e-e <88501808+m-a-d-e-e@users.noreply.github.com>
2021-09-02 01:13:31 +0530

    Add files via upload

---
+++ Products_NetStock_DataGrid/Products_NetStock_DataGrid/CalculateNetStock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Products_NetStock_DataGrid
+{
+    public class CalculateNetStock : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                Product product = value as Product;
+                decimal? netStock = product.UnitPrice * product.UnitsInStock;
+                return netStock.ToString();
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Products_NetStock_DataGrid/Products_NetStock_DataGrid/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Products_NetStock_DataGrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NorthwindEntities db = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            db = new NorthwindEntities();
+        }
+
+        private void MainGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+           ProductGrid.ItemsSource  = db.Products.ToList();
+        }
+    }
+}
