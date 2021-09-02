d47914d910f7b62e04492b67ea3b345b8d2cb104
m-a-d-e-e <88501808+m-a-d-e-e@users.noreply.github.com>
2021-09-02 23:42:19 +0530

    Add files via upload

---
+++ TemperatureApp_ICommand_INotify/TemperatureApp_ICommand_INotify/RelayCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TemperatureApp_ICommand_INotify
+{
+    public class RelayCommand : ICommand
+    {
+        readonly Action<object> _execute;
+        readonly Predicate<object> _canExecute;
+
+        //execute
+        public RelayCommand(Action<object> execute) : this(execute, null)
+        { }
+
+        //execute , canexecute
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)   // when to execute
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public void Execute(object paramter)
+        {
+            _execute(paramter);   //delegetae invoke - method invoke
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
---
+++ TemperatureApp_ICommand_INotify/TemperatureApp_ICommand_INotify/TemperatureClass.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TemperatureApp_ICommand_INotify
+{
+    public class TemperatureClass : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #region Fields and Properties 
+
+        private double tempValue;
+
+        public double TempValue
+        {
+            get { return tempValue; }
+            set { tempValue = value; PropertyChanged(this, new PropertyChangedEventArgs("TempValue")); }
+        }
+
+        private double celsiusValue;
+        public double CelsiusValue
+        {
+            get { return celsiusValue; }
+            set { celsiusValue = value; PropertyChanged(this, new PropertyChangedEventArgs("CelsiusValue")); }
+        }
+
+        private double fahreValue;
+        public double FahreValue
+        {
+            get { return fahreValue; }
+            set { fahreValue = value; PropertyChanged(this, new PropertyChangedEventArgs("FahreValue")); }
+        }
+
+        #endregion
+
+
+
+        #region Command Fields and Properties 
+
+        private RelayCommand _toCelsiusObj = null;
+
+        public ICommand ToCelsiusObj
+        {
+            get { return _toCelsiusObj; }
+        }
+
+        private RelayCommand _toFahreObj = null;
+
+        public ICommand ToFahreObj
+        {
+            get { return _toFahreObj; }
+        }
+
+        #endregion
+
+        #region Constructor and Methods
+
+        public TemperatureClass()
+        {
+            _toCelsiusObj = new RelayCommand(Convert_FtoC);
+            _toFahreObj = new RelayCommand(Convert_CtoF);
+        }
+
+        public void Convert_CtoF(object obj)
+        {
+            FahreValue = ((TempValue * 1.8) + 32);
+        }
+
+        public void Convert_FtoC(object obj)
+        {
+            CelsiusValue = ((TempValue - 32) * 0.5556);
+        }
+
+        #endregion
+
+    }
+}
