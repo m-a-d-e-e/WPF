bf2adf88fae813b87bcc6de9bf880ea3ff3771dd
m-a-d-e-e <88501808+m-a-d-e-e@users.noreply.github.com>
2021-09-05 17:11:46 +0530

    Add files via upload

---
+++ ComboBox_inMVVM/ComboBox_inMVVM/Models/Product.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComboBox_inMVVM.Models
+{
+    public class Product : INotifyPropertyChanged
+    {
+		private int productId;
+
+		public int ProductId
+		{
+			get { return productId; }
+			set { productId = value; OnPropertyChange("ProductId"); }
+		}
+
+		private string productName;
+
+		public string ProductName
+		{
+			get { return productName; }
+			set { productName = value; OnPropertyChange("ProductName"); }
+		}
+
+		private string category;
+
+		public string Category
+		{
+			get { return category; }
+			set { category = value; OnPropertyChange("Category"); }
+		}
+
+		private double price;
+
+		public double Price
+		{
+			get { return price; }
+			set { price = value; OnPropertyChange("Price"); }
+		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public void OnPropertyChange(string propName)
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propName));
+			}
+		}
+
+	}
+}
---
+++ ComboBox_inMVVM/ComboBox_inMVVM/ViewModels/ProductViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ComboBox_inMVVM.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace ComboBox_inMVVM.ViewModels
+{
+    public class ProductViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Product> productsList;
+
+        public ObservableCollection<Product> ProductsList
+        {
+            get { return productsList; }
+            set { productsList = value;}
+        }
+
+        private Product selectedProduct;
+        public Product SelectedProduct
+        {
+            get { return selectedProduct; }
+            set { selectedProduct = value; OnPropertyChange("SelectedProduct"); }
+        }
+
+        public ProductViewModel()
+        {
+            ProductsList = new ObservableCollection<Product>()
+            {
+                new Product { ProductId = 1, ProductName = "TV", Category = "Electronics", Price = 500000 },
+                new Product { ProductId = 2, ProductName = "Green tea", Category = "Tea", Price = 700 },
+                new Product { ProductId = 3, ProductName = "Black tea", Category = "Tea", Price = 570.5 },
+                new Product { ProductId = 4, ProductName = "Pudding", Category = "Desserts", Price = 1028.4 }
+            };
+        }
+      
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChange(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+    }
+}
