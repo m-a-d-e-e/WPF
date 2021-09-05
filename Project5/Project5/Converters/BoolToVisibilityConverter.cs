897adefd8599f09055cee83c63577c7bc44286c3
m-a-d-e-e <88501808+m-a-d-e-e@users.noreply.github.com>
2021-09-05 17:20:53 +0530

    Add files via upload

---
+++ Project5/Project5/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Project5.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+       
+        public Visibility TrueValue { get; set; }
+        
+        public Visibility FalseValue { get; set; }
+
+        public Visibility NullValue { get; set; }
+
+
+        public BoolToVisibilityConverter()
+        {
+            TrueValue = Visibility.Visible;
+            FalseValue = Visibility.Hidden;
+            NullValue = Visibility.Hidden;
+        }
+ 
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return NullValue;
+            }
+            bool b;
+            bool.TryParse(value.ToString(), out b);
+            return b ? TrueValue : FalseValue;
+        }
+
+        
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
